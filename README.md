5fdc8e99d488a2cd5ca0e109b2383bca4433042e
Justin Taormina <jstaormina@gmail.com>
2019-03-11 21:35:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# WERC Marathon Training plan scheduler
+
+## Prerequisites
+Go [here](https://developers.google.com/calendar/quickstart/nodejs) and follow Step 1, download your ``credentials.json``
+to the root of this directory. *DO NOT ADD THIS FILE TO SOURCE CONTROL.  DELETE IT WHEN YOU'RE DONE*
+
+Make sure that you have NodeJS and NPM installed.
+
+## Usage
+
+```bash
+npm install
+
+npm start
+```
+
+Follow the terminal prompts to grant google calendar access to this script.  The app should then run to completion.  
+Revoke access after the script has finished and delete the `token.json` file that is created.
+
+###
+Fin
---
+++ app.js
@@ -0,0 +1,129 @@
+const Excel = require('exceljs');
+const moment = require('moment');
+const readline = require('readline');
+const google = require('googleapis').google;
+const fs = require('fs');
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
+const TOKEN_PATH = 'token.json';
+
+const colorMap = {
+    'FFBFBFBF': 'HR1',
+    'FFC0C0C0': 'HR1',
+    'FF00FFFF': 'HR2',
+    'FFFFFF00': 'HR4',
+    'FFFF99CC': 'HR5a',
+    'FFFF0000': 'HR5b',
+    'FF00FF00': 'HR various',
+
+};
+
+(async () => {
+    const events = await parseXlsx();
+    updateCalendarWithEvent(events);
+})();
+
+async function parseXlsx() {
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile('./resources/ATC_RUN_SCHEDULES.xlsx');
+    const worksheet = workbook.getWorksheet('Full_Marathon');
+    const calEntries = [];
+    worksheet.eachRow({}, (row, rowNumber) => {
+        if (rowNumber > 8 && rowNumber < 28) {
+            const startDate = row.values[1].result;
+            row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+                if (colNumber > 3 && colNumber < 11 && cell.value !== 'OFF') {
+                    const calEntry = {
+                        summary: 'Marathon Training',
+                        start: {
+                            dateTime: moment(startDate).add(colNumber - 3, 'days').hours(7).toDate()
+                        },
+                        end: {
+                            dateTime: moment(startDate).add(colNumber - 3, 'days').hours(8).toDate()
+                        }
+                    };
+                    const hr = cell.style.fill ? colorMap[cell.style.fill.fgColor.argb] : 'HR3';
+                    if (colNumber === 4 || colNumber === 9) {
+                        calEntry.description = 'Run ' + cell.value + ' miles at ' + hr;
+                    } else if (colNumber === 5) {
+                        calEntry.description = 'Run ' + cell.value + ' minutes at ' + hr;
+                    } else if (colNumber === 6) {
+                        calEntry.description = 'Bike ' + cell.value + ' hour at ' + hr;
+                    } else if (colNumber === 7 && cell.value !== 1) {
+                        calEntry.description = 'low intensity recovery workout ' + cell.value + ' minutes at ' + hr;
+                    } else if (colNumber === 7) {
+                        calEntry.description = 'Bootcamp or track ' + cell.value + ' hour at ' + hr;
+                    } else if (cell.value === 'XT') {
+                        calEntry.description = 'Crosstrain';
+                    }
+
+                    calEntries.push(calEntry);
+                }
+            });
+        }
+    });
+
+    return calEntries;
+}
+
+function updateCalendarWithEvent(events) {
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        authorize(JSON.parse(content), (auth) => {
+            createEvents(auth, events);
+        });
+    });
+}
+
+function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+function createEvents(auth, workoutEvents) {
+    const calendar = google.calendar({version: 'v3', auth});
+
+    workoutEvents.forEach((event) => {
+        calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+        }, (err, res) => {
+            if (err) {
+                console.log('Error: ' + err);
+                return
+            }
+            console.log('New event created: ' + res.data.htmlLink);
+        })
+    });
+}
